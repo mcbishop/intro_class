8bc7d24748a10caf03423c30468fb4b22382c977
Meow Bishop <mchilton@gmail.com>
2015-11-29 15:32:53 -0800

    intro python/data viz: arrays and scalars

---
+++ arrays_scalars.py
@@ -0,0 +1,77 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import numpy as np
+
+
+# In[2]:
+
+1/2
+
+
+# In[3]:
+
+5/2
+
+
+# In[4]:
+
+5.0/2
+
+
+# In[5]:
+
+from__future__import division
+
+
+# In[7]:
+
+from __future__ import division
+
+
+# In[11]:
+
+arr1 = np.array([[1,2,3,4],[8,9,10,11]])
+
+
+# In[12]:
+
+arr1
+
+
+# In[13]:
+
+arr1*arr1
+
+
+# In[14]:
+
+arr1 - arr1
+
+
+# In[15]:
+
+1 / arr1
+
+
+# In[16]:
+
+1/1
+
+
+# In[17]:
+
+1/2
+
+
+# In[18]:
+
+arr1 ** 3
+
+
+# In[ ]:
+
+
+
