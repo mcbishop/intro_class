eef3cfdb17b9140a855785b81bf4c98dd73d9ab3
Meow Bishop <mchilton@gmail.com>
2015-12-08 21:31:15 -0800

    incomplete - most expensive item in dictionary

---
+++ expensive.py
@@ -0,0 +1,17 @@
+# Given a dictionary of items and their prices, write a program that returns the most expensive 
+#item.
+
+prices = {"banana":4,"apple":5,"orange":1.5,"pear":3}
+
+# look at each price of each item
+# compare banana price to apple, orange, pear
+# if it's bigger than everything, print key and price
+# else, go to the next item and compare it to the rest of the list
+
+for fruit in prices:
+	if banana[value]<apple[value]:
+		go to next item, repeat
+
+
+
+# biggest price wins so we want answer to be apple 
