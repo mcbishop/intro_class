ae2fa8a65fe07b991535051b302420d949e22289
Meow Bishop <mchilton@gmail.com>
2015-12-10 00:41:56 -0800

    completed problem!

--- expensive.py
+++ expensive.py
@@ -3,15 +3,14 @@
 
 prices = {"banana":4,"apple":5,"orange":1.5,"pear":3}
 
-# look at each price of each item
-# compare banana price to apple, orange, pear
-# if it's bigger than everything, print key and price
-# else, go to the next item and compare it to the rest of the list
+# look at value of each item. Compare it to the next. Pick the largest item and compare it to the next. Continue until no more items exist and print the largest item.
 
-for fruit in prices:
-	if banana[value]<apple[value]:
-		go to next item, repeat
+fruit = ""
+biggest = 0
 
-
-
-# biggest price wins so we want answer to be apple 
+for x in prices: 
+	cost = prices[x] #grabs value of each item
+	if cost > biggest:
+		biggest = cost
+		fruit = x 
+print fruit, biggest
