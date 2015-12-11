98875da7eda4d2b6b72f58ef5b701bfbabc5369d
Meow Bishop <mchilton@gmail.com>
2015-12-10 21:46:20 -0800

    outline for rock paper scissors

---
+++ rock_paper_scissors.py
@@ -0,0 +1,14 @@
+#write a terminal-based rock, paper scissors program
+# user vs user?
+# user vs computer?
+
+#how rock paper scissors works:
+#either: randomly generate 2 out of three for two variables:
+#or: ask users for input of rock, paper and scissors
+
+#if rock and scissors: rock wins
+#if rock and paper: paper wins
+#if rock and rock: tie
+#if paper and scissors: scissors wins
+# if scissors and scissors: tie
+# if paper and paper: tie
