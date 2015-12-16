19cebbc6d558f7841bbc23c737681063d546fe1e
Meow Bishop <mchilton@gmail.com>
2015-12-15 16:44:21 -0800

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,14 +1,19 @@
-#write a terminal-based rock, paper scissors program
-# user vs user?
-# user vs computer?
 
-#how rock paper scissors works:
-#either: randomly generate 2 out of three for two variables:
-#or: ask users for input of rock, paper and scissors
+one = raw_input("Player 1: Give me a value: rock, paper, or scissors. ")
+two = raw_input("Player 2: Give me a value: rock, paper, or scissors. ")
 
-#if rock and scissors: rock wins
-#if rock and paper: paper wins
-#if rock and rock: tie
-#if paper and scissors: scissors wins
-# if scissors and scissors: tie
-# if paper and paper: tie
+def rock_paper_scissors(player1,player2):
+	if (player1 == "rock" and player2 == "scissors") or (player2 == "rock" and player1 == "scissors"):
+    		print "Rock wins. Rock smashes scissors."
+	elif (player1 == "rock" and player2 == "paper") or (player2 == "rock" and player1 == "paper"):
+			print "Paper wins. Paper wraps up rock."
+	elif player1 == player2:
+	 		print "It's a tie. "
+	elif (player1 == "paper" and player2 == "scissors") or (player2 == "paper" and player1 == "scissors"):
+	 		print "Scissors wins. Scissors cuts through paper."
+
+
+
+
+
+rock_paper_scissors(one,two)
